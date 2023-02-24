fa07286378517aab2b970864f62aacc3d64d036d
mageshcoder <codermagesh@gmail.com>
2023-02-24 16:42:34 +0530

    feb 24 commit

--- lib/opening_splash_screen.dart
+++ lib/opening_splash_screen.dart
@@ -14,74 +14,83 @@ class _SplashState extends State<Splash> {
     double myHeight = MediaQuery.of(context).size.height;
     double myWidth = MediaQuery.of(context).size.width;
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: SizedBox(
-          height: myHeight,
-          width: myWidth,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Image.asset('loading1.gif'),
-              Column(
-                children: const [
-                  Text(
-                    'The Future',
-                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Learn more about cryptocurrency, look to',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.grey),
-                  ),
-                  Text(
-                    ' the future in IO Crypto',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.grey),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: myWidth * 0.14),
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginScreenPage()));
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: const Color(0xffFBC700),
-                        borderRadius: BorderRadius.circular(50)),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: myWidth * 0.05,
-                          vertical: myHeight * 0.013),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'CREATE PORTFOLITO  ',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.normal,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark),
+        home: Scaffold(
+          body: SizedBox(
+            height: myHeight,
+            width: myWidth,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.network(
+                  "https://media.tenor.com/Qzr6WpBLiEAAAAAi/technology-tech.gif",
+                  width: 200, // set the width of the image
+                  height: 200, // set the height of the image
+                  fit: BoxFit
+                      .contain, // set the fit property to contain or cover
+                ),
+                Column(
+                  children: const [
+                    Text(
+                      'The Future',
+                      style:
+                          TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Learn more about cryptocurrency, look to \n the future in IO Crypto',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.grey),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: myWidth * 0.14),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreenPage()));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: const Color(0xffFBC700),
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: myWidth * 0.05,
+                            vertical: myHeight * 0.013),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text(
+                              'CREATE PORTFOLITO  ',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal,
+                              ),
                             ),
-                          ),
-                          RotationTransition(
-                              turns: AlwaysStoppedAnimation(310 / 360),
-                              child: Icon(Icons.arrow_forward_rounded))
-                        ],
+                            RotationTransition(
+                                turns: AlwaysStoppedAnimation(310 / 360),
+                                child: Icon(Icons.arrow_forward_rounded))
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
