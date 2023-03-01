e39c629ea8eceae4ab9984aa602139d9715afd9a
mageshcoder <codermagesh@gmail.com>
2023-03-01 17:40:47 +0530

    added chatbot successfully

--- lib/screens/chat_bot_new.dart
+++ lib/screens/chat_bot_new.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import, library_private_types_in_public_api
+
 import 'dart:async';
 
 import 'package:chat_gpt_flutter/chat_gpt_flutter.dart';
@@ -29,18 +31,17 @@ class _ChatScreensState extends State<ChatScreens> {
       headers: {
         'Content-Type': 'application/json',
         'Authorization':
-            'Bearer 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',
+            'Bearer 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',
       },
       body: jsonEncode(
         {
           'prompt':
-              'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n:\n  $message ',
-          'temperature': 0.3,
-          'max_tokens': 30,
+              'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nUser: $message\n',
+          'temperature': 0.5,
+          'max_tokens': 15,
           'top_p': 1,
           'frequency_penalty': 0,
-          'presence_penalty': 0,
-          // 'n': 1,
+          'presence_penalty': 0
         },
       ),
     );
@@ -53,12 +54,15 @@ class _ChatScreensState extends State<ChatScreens> {
     }
 
     final completions = responseBody['choices'][0]['text'];
-    print(completions);
+    final textBeforeNewline = completions.split('\n')[0];
 
-    return completions;
+    return textBeforeNewline;
   }
 
   Future<void> _handleSubmitted(String text) async {
+    setState(() {
+      _messages.add(ChatMessage(message: text, isUserMessage: true));
+    });
     final response = await getResponse(text);
     setState(() {
       _messages.add(ChatMessage(message: response, isUserMessage: false));
@@ -82,6 +86,12 @@ class _ChatScreensState extends State<ChatScreens> {
                 return ChatBubble(
                   message: message.message,
                   isUserMessage: message.isUserMessage,
+                  backgroundColor: message.isUserMessage
+                      ? const Color.fromARGB(255, 244, 215, 173)
+                      : Colors.black,
+                  textColor: message.isUserMessage
+                      ? const Color.fromARGB(255, 11, 11, 11)
+                      : Colors.white,
                 );
               },
             ),
@@ -99,42 +109,36 @@ class _ChatScreensState extends State<ChatScreens> {
 class ChatBubble extends StatelessWidget {
   final String message;
   final bool isUserMessage;
+  final Color backgroundColor;
+  final Color textColor;
 
   const ChatBubble({
     Key? key,
     required this.message,
     required this.isUserMessage,
+    required this.backgroundColor,
+    required this.textColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return isUserMessage
-        ? Align(
-            alignment: Alignment.topRight,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-              ),
-              padding: const EdgeInsets.all(8.0),
-              margin:
-                  const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-              child: Text(message),
-            ),
-          )
-        : Align(
-            alignment: Alignment.topLeft,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-              ),
-              padding: const EdgeInsets.all(8.0),
-              margin:
-                  const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-              child: Text(message),
-            ),
-          );
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+      child: Align(
+        alignment: isUserMessage ? Alignment.topRight : Alignment.topLeft,
+        child: Container(
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+          ),
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            message,
+            style: TextStyle(color: textColor),
+          ),
+        ),
+      ),
+    );
   }
 }
 
